9106eb3ed6664a3719046f4ba70621fbb91c0940
Isaac Lubanko <isaaclubanko@MacBook-Pro-2.fios-router.home>
2018-03-06 21:48:19 -0500

    reformatted code to object oriented and added timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,6 +16,15 @@
 //when you click the start over button it restarts the game WITHOUT reloading the page. 
 
 var triviaGame = {
+    //object global variables
+    questionArray : [],
+    counter : 0,
+    maxQuestions : 3,
+    interval : 10000,
+    correctGuesses : 0,
+    wrongGuesses : 0,
+    lateGuesses : 0,
+    currentQuestion : undefined,
     questions : {
         //maybe use "q1", "q2", for properties and have a questionText for each property
         "q1" : {
@@ -85,76 +94,116 @@ var triviaGame = {
         }
     },
 
-}
-//throws the question properties (q1,q2) into an array for selecting
+    initialize : function() {
+       triviaGame.questionArray = Object.keys(triviaGame.questions);
+       triviaGame.counter = 0;
+    },
 
-var questionArray = [];
-var counter = 0;
-var maxQuestions = 3;
-var interval = 10000;
-var correctGuesses;
-var wrongGuesses;
-var lateGuesses;
-var currentQuestion;
+    displayQuestion: function (){
+        triviaGame.timer.start();
+        //creates a random number to select a question from the array
+        var questionCounter = Math.floor(Math.random() * (triviaGame.questionArray.length));
+        //plugs that random number into the questions property object
+        triviaGame.currentQuestion = triviaGame.questions[triviaGame.questionArray[questionCounter]];
+        //removes item at the index from the question array
+        triviaGame.questionArray.splice(questionCounter, 1); 
+        //LOGS: REMOVE
+        //console.log("current question: " + triviaGame.currentQuestion);
+        //console.log("question counter " + triviaGame.questionCounter);
+        //console.log("question array " + triviaGame.questionArray);
+        //empties the relevant DOM area
+        $("#question").empty();
+        $("#answers").empty();
 
-function initializeGame(){
-    questionArray = Object.keys(triviaGame.questions);
-    counter = 0;
-}
-
-//TODO: get game to display questions in HTML before working on the timer part;
-function displayQuestion(){
-    $("#question").empty();
-    $("#answers").empty();
-    $("#question").append("<p>" + currentQuestion.questionText + "</p>");
-    for(i = 0; i < currentQuestion.answers.length; i++){
-        $("#answers").append("<button class = 'option' data-number = " + i + "> " + currentQuestion.answers[i].text + "</button>");
-    }
-
-    $(".option").on("click", function(){
-        var answerRef = $(this).data("number");
-        //REMOVE THESE//
-        if(currentQuestion.answers[answerRef].correct){
-            console.log("Correct!");
-            correctGuesses++;
-        } else {
-            console.log("wrong answer m8")
-            wrongGuesses++;
+        //appends the text of the question to the div 
+        $("#question").append("<p>" + triviaGame.currentQuestion.questionText + "</p>");
+        //for loop that cycles through the answers and displays them on the page 
+        for(i = 0; i < triviaGame.currentQuestion.answers.length; i++){
+            $("#answers").append("<button class = 'option' data-number = " + i + "> " + triviaGame.currentQuestion.answers[i].text + "</button>");
         }
-        //REMOVE THESE//
-    })
-};
+    
+        $(".option").on("click", function(){
+            var answerRef = $(this).data("number");
+            var result = ""
+            //REMOVE THESE//
+            if(triviaGame.currentQuestion.answers[answerRef].correct){
+                result = "Correct!";
+                triviaGame.correctGuesses++;
+            } else if (triviaGame.currentQuestion.answers[answerRef].correct) {
+                result = "Wrong!"
+                triviaGame.wrongGuesses++;
+            } else if(triviaGame.timer.timeup) {
+                result = "Times up jack!"
+                triviaGame.lateGuesses++;
+            };
+            triviaGame.timer.reset();
+            $("#gameboard").text("<p>" + result +"</p>")
+            setTimeout(triviaGame.startGame, 2000);
+
+            //REMOVE THESE//
+        });
+    },
+
+    endScreen: function (){
+        $("#game-board").empty();
+        $("#game-board").append("<p>Correct Guesses: " + triviaGame.correctGuesses + "</p>");
+        $("#game-board").append("<p>Wrong Guesses: " + triviaGame.wrongGuesses + "</p>");
+    },
+
+    startGame: function() {
+
+        //first checks if the counter has reached the max amount of questions
+        if(triviaGame.counter < triviaGame.maxQuestions){
+            triviaGame.displayQuestion();
+            triviaGame.counter++;
+            //setTimeout(triviaGame.startGame, triviaGame.interval);
+
+        } else {
+            triviaGame.endScreen();
+        }    
+
+},
+    
+
+    timer : {
+        countdown : 7,
+        timeup: false,
+        start : function() {
+            countdownID = setInterval(function(){
+                triviaGame.timer.countdown --; 
+                if(triviaGame.timer.countdown <= 0){
+                    clearInterval(countdownID);
+                    triviaGame.timer.timeup = true;
+                };
+                $("#timer").text(triviaGame.timer.countdown);
+
+            }, 1000);
+
+        },
+        
+        reset: function() {
+            clearInterval(countdownID);;
+            triviaGame.timer.countdown = 27;
+            $("#timer").empty();
+            triviaGame.timer.timeup = false;
+        }
+        
+
+        
+    }    
+    
+
+
+}
+
+
+
+
 
 //the basic question generation mechanism
-function startGame() {
 
-            //first checks if the counter has reached the max amount of questions
-            if(counter < maxQuestions){
-                //if not it creates a new counter to select a question by picking a random number between 0 and the question length 
-                var questionCounter = Math.floor(Math.random() * (questionArray.length));
-                //inputs that question counter into the questionArray key, then inputs that key into the trivia.questions object to retrieve the text of a question
-                currentQuestion = triviaGame.questions[questionArray[questionCounter]];
-                //logs: REMOVE THESE
-                console.log(currentQuestion);
-                console.log(questionCounter);
-                console.log(questionArray);
-                //REMOVE THESE 
-                displayQuestion();
-
-                questionArray.splice(questionCounter, 1); 
-                if(counter <= maxQuestions) {
-                    counter++;
-                    setTimeout(startGame, interval);
-                } 
-
-            } else {
-                console.log("nope");
-            }    
-
-     //displays question after a certain time
-}
 
 $("#startgame").on("click", function(){
-    initializeGame();
-    startGame();
-});
+    triviaGame.initialize();
+    triviaGame.startGame();
+})
